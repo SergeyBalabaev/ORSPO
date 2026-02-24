ca1265d1a3e1102509de64dbdd554a2ddd7f2473
SergeyBalabaev <sergei.balabaev@mail.ru>
2026-02-24 20:43:16 +0300

    correct task8

--- Task008/main.c
+++ Task008/main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main()
+
+int main(){
+int x = 5;
+    int arr[100];
+arr[1000] = 20;
+    printf("Hello");
+if (x > 3) 
 {
-	int x = 5;
-	int arr[100];
-	arr[1000] = 20;
-	printf("Hello");
-	if (x > 3) {
-		printf("World");
-	}
-	return 0;
+printf("World");
+}
+return 0;
 }
