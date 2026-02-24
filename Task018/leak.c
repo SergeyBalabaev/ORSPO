ddcf6fe80671c88377a9aeb51029f076bdce327b
SergeyBalabaev <sergei.balabaev@mail.ru>
2026-02-24 21:08:22 +0300

    add

--- Task018/leak.c
+++ Task018/leak.c
@@ -1,39 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void create_leak() {
-    int *arr = (int*)malloc(10 * sizeof(int));
-    // Забыли освободить память!
-    for (int i = 0; i < 10; i++) {
-        arr[i] = i;
-    }
-    printf("Array created, but not freed\n");
-}
-
-void use_uninitialized() {
-    int *ptr = (int*)malloc(sizeof(int));
-    // Используем неинициализированную память
-    if (*ptr > 0) {
-        printf("Value is positive\n");
-    }
-    free(ptr);
-}
-
-void write_out_of_bounds() {
-    int *arr = (int*)malloc(5 * sizeof(int));
-    for (int i = 0; i <= 5; i++) { // Ошибка: выход за границы при i=5
-        arr[i] = i * 10;
-    }
-    free(arr);
-}
-
 int main() {
-    printf("=== Valgrind Demo ===\n");
-    
-    create_leak();
-    use_uninitialized();
-    write_out_of_bounds();
-    
-    printf("Program finished\n");
+    int N_mas = 10;
+    double** A = (double**)malloc(N_mas * sizeof(double*));
+    for (int i = 0; i < N_mas; i++) {
+        A[i] = (double*)malloc(N_mas * sizeof(double));
+    }
+
+    free(A);
     return 0;
 }
+
